4cb9a7a5b26489eccba6505b1c09be2e661b3327
Aleksandrs Larionovs <admin@aleksandrslarionovs.com>
2023-12-05 20:32:12 +0000

    fefefe

---
+++ sql/tags.sql
@@ -0,0 +1,7 @@
+CREATE TABLE tags (
+    tag_id INT PRIMARY KEY AUTO_INCREMENT,
+    tag_name VARCHAR(50) NOT NULL,
+    post_id INT,
+    FOREIGN KEY (post_id) REFERENCES posts(post_id)
+    -- Add more columns as needed for additional post information
+);
---
+++ sql/users_topics.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users_topics (
+    user_topic INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT,
+    topic_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (topic_id) REFERENCES topics(post_id)
+    
+    -- Add more columns as needed for additional post information
+);
