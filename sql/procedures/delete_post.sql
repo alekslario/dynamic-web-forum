cab5a4f0c89de2174be731bbf418b40a1f4e5738
Aleksandrs Larionovs <admin@aleksandrslarionovs.com>
2023-12-05 21:44:56 +0000

    Update post procedure

--- sql/procedures/delete_post.sql
+++ sql/procedures/delete_post.sql
@@ -6,6 +6,8 @@ BEGIN
     -- Delete replies associated with the post
     DELETE FROM replies WHERE post_id = postIdToDelete;
 
+    DELETE FROM tags WHERE post_id = postIdToDelete;
+
     -- Delete the post
     DELETE FROM posts WHERE post_id = postIdToDelete;
 
