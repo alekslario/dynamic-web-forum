9afba72a89b558d3e49c6e08f0b0ce9e6b6f8820
Aleksandrs Larionovs <admin@aleksandrslarionovs.com>
2023-12-05 20:36:04 +0000

    Revert "fefefe"
    This reverts commit 4cb9a7a5b26489eccba6505b1c09be2e661b3327.

--- sql/topics.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE topics (
-    topic_id INT PRIMARY KEY AUTO_INCREMENT,
-    topic_name VARCHAR(50) NOT NULL,
-    details TEXT NOT NULL,  
-    -- Add more columns as needed for additional post information
-);
