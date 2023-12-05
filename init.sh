9f652417f6a100f460af90c6077c6cc0cfcbf0de
Aleksandrs Larionovs <admin@aleksandrslarionovs.com>
2023-12-05 22:07:34 +0000

    ACreate init bash file

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# create random password
+PASSWDDB="123"
+MAINDB="myBookshop"
+# replace "-" with "_" for database username
+USER="alari001"
+#create database
+sudo mysql <<MYSQL_SCRIPT
+CREATE USER ${USER}@localhost IDENTIFIED BY '${PASSWDDB}';
+CREATE DATABASE IF NOT EXISTS  ${MAINDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;
+GRANT ALL PRIVILEGES ON ${MAINDB}.* TO '${USER}'@'localhost';
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
