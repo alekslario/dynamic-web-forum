23cac94bc86add243cb11c5e87064639942b5fc9
Aleksandrs Larionovs <admin@aleksandrslarionovs.com>
2023-12-05 22:05:41 +0000

    Add sql files to master files

---
+++ master.sql
@@ -0,0 +1,5 @@
+source sql/init/master.sql
+source sql/procedures/delete_post.sql
+source sql/views/post_view.sql
+source sql/views/replies_view.sql
+source sql/views/posts_list_view.sql
---
+++ sql/init/master.sql
@@ -0,0 +1,6 @@
+source users.sql
+source topics.sql
+source posts.sql
+source replies.sql
+source tags.sql
+source users_topics.sql
