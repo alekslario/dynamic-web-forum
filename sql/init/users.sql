203afa92e7d19d72544db3080c02b94d8080d3da
Aleksandrs Larionovs <admin@aleksandrslarionovs.com>
2023-12-05 20:50:04 +0000

    Move sql tables in init folder

--- sql/init/users.sql
+++ sql/init/users.sql
@@ -2,6 +2,7 @@ CREATE TABLE users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
+    about TEXT,
     -- Generally, you should not store passwords in plain text, but for this example we will
     password_hash VARCHAR(25) NOT NULL,
 
